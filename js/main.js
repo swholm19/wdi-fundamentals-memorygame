1f205dd2bbe21b234169b1905b257680f188aad8
TDMK Digital <tdmkdigital@TDMKs-MacBook-Pro.local>
2018-05-14 23:16:15 -0400

    created effiecent game logic

--- js/main.js
+++ js/main.js
@@ -2,23 +2,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push( cardOne ); 
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push( cardTwo );
-
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again.")
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(2);
+flipCard(0);
+console.log(cardsInPlay);
