091a2cb2b88b5f747a752770bb64fdfa00ca06dc
TDMK Digital <tdmkdigital@TDMKs-MacBook-Pro.local>
2018-05-16 23:37:53 -0400

    Final Product, added card image flip

--- js/main.js
+++ js/main.js
@@ -20,27 +20,30 @@ var cards = [{
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	var src = this.setAttribute(cards.cardImage); //wrong!!!
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again.");
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
 	}
 }
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id'); //check
-	//document.setAttribute(cards[cardId].cardIamge);  //check
+	var cardId = this.getAttribute('data-id'); //What is "this" in reference too... what object???
+	
+	//Still not sure what "this" is calling...
+	//Why when you click the second card this is so slow to update the picture that the alert appears first?
+	this.setAttribute('src', cards[cardId].cardImage);
 
-	console.log("User flipped " + cards[cardId].cardIamage);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
+	//Why doesnt this code below work but "this" above does...
+	//document.getElementById('game-board')[cardId].setAttribute('src', cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId].rank);
 
-	checkForMatch();
+	//Moved if statement that checks for length to flipCard function because i dont want to go to
+	//checkForMatch function until i have atleast two cards
+	if (cardsInPlay.length == 2) { 
+		checkForMatch();
+	}
 }
 
 var createBoard = function() {
@@ -48,12 +51,13 @@ var createBoard = function() {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
-		cardElement.addEventListener('click', flipCard);
+		cardElement.addEventListener('click', flipCard); //Why does this need to be inside for loop?
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
-//flipCard(1);
-//flipCard(0);
 createBoard();
-console.log(cardsInPlay);
+
+
+
+
